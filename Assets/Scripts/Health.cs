35e9a1cbd9f4df45a1abdbf0f3743b4d47e626f4
kkuloo <kkulooo@naver.com>
2024-11-09 04:58:43 +0900

    Initial commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public float hp = 10;
+    public IHealthListener healthListener;
+    public float invincibleTime;
+    public AudioClip hitSound;
+    public AudioClip dieSound;
+
+    public Image hpGauge;
+
+    float maxHP;
+    float lastAttackedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHP = hp;
+        healthListener = GetComponent<Health.IHealthListener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(float damage)
+    {
+        if (hp > 0 && lastAttackedTime + invincibleTime < Time.time )
+        {
+            hp -= damage;
+            if (hpGauge !=null)
+            {
+                hpGauge.fillAmount = hp / maxHP;
+            }
+
+            lastAttackedTime = Time.time;
+
+            if (hp <= 0)
+            {
+                if (dieSound != null) 
+                {
+                    GetComponent<AudioSource>().PlayOneShot(dieSound);
+                }
+                if (healthListener != null)
+                {
+                    healthListener.Die();
+                }
+            }
+            else
+            {
+                if (hitSound != null)
+                {
+                    GetComponent<AudioSource>().PlayOneShot(hitSound);
+                }
+            }
+        }
+    }
+
+    public interface IHealthListener 
+    {
+        void Die();
+    }
+}
